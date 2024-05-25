592e307dddfa4ad9e4b76795ae3980e2e56ecf67
Maria Tavares <mariaeduardtavares@gmail.com>
2024-05-25 10:36:12 -0300

    Modificações na semântica main.js

--- js/main.js
+++ js/main.js
@@ -1,42 +1,52 @@
-$('#telefone').mascara('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
-})
+$(document).ready(function () {
+    $('#carousel-imagens').slick({
+        autoplay: true,
+        arrows: false
+    })
 
-$('#cpf').mascara('000.000.000-00', {
-    placeholder: '123.456.789-00'
-})
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(DDD) 12345-6789'
+    })
 
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
-})
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '123.456.789-00'
+    })
 
-$('form').validate({
-    rules: {
-        nome: {
-            requerido: true
+    $('#cep').mask('00000-000', {
+        placeholder: '012345-678'
+    })
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
         },
-        email: {
-            requerido: true,
-            email: true
+        messages: {
+            nome: 'Por favor, insira o seu nome.'
         },
-        telefone: {
-            requerido: true
+        submitHandler: function(form) {
+            alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
+            form.reset();    
         },
-        endereco: {
-            requerido: true
-        },
-        cep: {
-            requerido: true
-        },
-        cpf: {
-            requerido: true
-        },
-    },
-    submitHandler: function (form) {
-        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-        form.reset();
-    },
-    invalidHandler: function (form, validator) {
-        alert("Por favor, preencha os campos para prosseguir com a compra!");
-    }
-})
+        invalidHandler: function(form, validador) {
+                alert("Por favor, preencha os campos para prosseguir com a compra!");
+        }
+    })
+});
