bad61d21d8578ac6872e13c65d09de8c25e35cbb
David Carvalho <davidsnv@gmail.com>
2024-06-27 22:49:43 -0300

    resolucao_modulo10

--- js/main.js
+++ js/main.js
@@ -1,42 +1,82 @@
-$('#telefone').mascara('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
-})
+$(document).ready(function () {
+   $("#carousel-imagens").slick({
+      autoplay: true,
+      arrows: false,
+   });
+   $("#telefone").mask("(00) 00000-0000", {
+      placeholder: "(DDD) 12345-6789",
+   });
 
-$('#cpf').mascara('000.000.000-00', {
-    placeholder: '123.456.789-00'
-})
+   $("#cpf").mask("000.000.000-00", {
+      placeholder: "123.456.789-00",
+   });
 
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
-})
+   $("#cep").mask("00000-000", {
+      placeholder: "01234-567",
+   });
 
-$('form').validate({
-    rules: {
-        nome: {
-            requerido: true
-        },
-        email: {
-            requerido: true,
-            email: true
-        },
-        telefone: {
-            requerido: true
-        },
-        endereco: {
-            requerido: true
-        },
-        cep: {
-            requerido: true
-        },
-        cpf: {
-            requerido: true
-        },
-    },
-    submitHandler: function (form) {
-        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-        form.reset();
-    },
-    invalidHandler: function (form, validator) {
-        alert("Por favor, preencha os campos para prosseguir com a compra!");
-    }
-})
+   $("form").validate({
+      rules: {
+         nome: {
+            required: true,
+         },
+         email: {
+            required: true,
+            email: true,
+         },
+         telefone: {
+            required: true,
+         },
+         endereco: {
+            required: true,
+         },
+         cep: {
+            required: true,
+         },
+         cpf: {
+            required: true,
+         },
+      },
+      messages: {
+         nome: "Digite seu nome completo",
+         email: "Digite seu e-mail",
+         telefone: "Digite seu telefone",
+         cpf: "Digite seu CPF",
+         endereco: "Digite seu endereço completo",
+         cep: "Digite seu CEP",
+      },
+      submitHandler: function (form) {
+         const inputNameValue = $("#nome").val();
+
+         alert(
+            `Olá ${inputNameValue}, sua requisição foi enviada para análise, parabéns pela aquisição!`
+         );
+         form.reset();
+      },
+      invalidHandler: function (form, validator) {
+         const inputNameValue = $("#nome").val();
+         const numberOfFild = validator.numberOfInvalids();
+
+         if (numberOfFild == 1) {
+            alert(
+               `Por favor ${inputNameValue}, preencha o campo para prosseguir com a compra!`
+            );
+         } else {
+            alert(
+               `Por favor ${inputNameValue}, preencha os ${numberOfFild} campos para prosseguir com a compra!`
+            );
+         }
+      },
+   });
+
+   $("header").click(function () {
+      const destino = $("#container");
+
+      $("html").animate(
+         {
+            scrollTop: destino.offset().top,
+         },
+         1000
+      );
+   });
+});
