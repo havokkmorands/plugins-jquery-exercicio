56c8d92019918420b25fd1cf7a4e1d3f6d22a589
deMuNeS <136003237+deMuNeS@users.noreply.github.com>
2024-07-22 21:51:01 -0300

    exercicio

--- js/main.js
+++ js/main.js
@@ -1,40 +1,38 @@
-$('#telefone').mascara('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
+$('#telefone').mask('(00) 00000-0000', {
 })
 
-$('#cpf').mascara('000.000.000-00', {
-    placeholder: '123.456.789-00'
+$('#cpf').mask('000.000.000-00', {
 })
 
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
-})
+$('form').validate
+    $("#cep").mask("00000-000", {
+    });
 
 $('form').validate({
     rules: {
         nome: {
-            requerido: true
+            required: true
         },
         email: {
-            requerido: true,
+            required: true,
             email: true
         },
         telefone: {
-            requerido: true
+            required: true
         },
         endereco: {
-            requerido: true
+            required: true
         },
         cep: {
-            requerido: true
+            required: true
         },
         cpf: {
-            requerido: true
+            required: true
         },
     },
     submitHandler: function (form) {
         alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-        form.reset();
+        form();
     },
     invalidHandler: function (form, validator) {
         alert("Por favor, preencha os campos para prosseguir com a compra!");
