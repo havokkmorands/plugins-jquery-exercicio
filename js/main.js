c54675901f4d8b5904d027737844780162d133d8
Mateus Lino <mateuspireslino@gmail.com>
2024-11-03 22:14:24 -0300

    feat: validação do formulario e correção do carrossel

--- js/main.js
+++ js/main.js
@@ -1,42 +1,59 @@
-$('#telefone').mascara('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
+$(document).ready(function(){
+    $('#carousel-imagens').slick({
+        autoplay: true
+    });
+
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(DDD) 12345-6789'
+    });
+
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '123.456.789-00'
+    });
+
+    $('#cep').mask('00000-000', {
+        placeholder: '012345-678'
+    });
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+            cpf: {
+                required: true
+            }
+        },
+
+        messages: {
+            nome: 'Por favor, insira seu nome completo',
+            email: 'Por favor, insira um e-mail valido',
+            telefone: 'Por favor, insira um número valido',
+            cpf: 'Por favor, insira um CPF valido',
+            endereco: 'Por favor, insira um endereço',
+            cep: 'Por favor, insira um CEP valido',
+        },
+
+        submitHandler: function(form) {
+            alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
+            form.reset();
+        },
+        invalidHandler: function(form, validator) {
+            alert("Por favor, preencha os campos para prosseguir com a compra!");
+        }
+    });
 })
 
-$('#cpf').mascara('000.000.000-00', {
-    placeholder: '123.456.789-00'
-})
 
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
-})
-
-$('form').validate({
-    rules: {
-        nome: {
-            requerido: true
-        },
-        email: {
-            requerido: true,
-            email: true
-        },
-        telefone: {
-            requerido: true
-        },
-        endereco: {
-            requerido: true
-        },
-        cep: {
-            requerido: true
-        },
-        cpf: {
-            requerido: true
-        },
-    },
-    submitHandler: function (form) {
-        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-        form.reset();
-    },
-    invalidHandler: function (form, validator) {
-        alert("Por favor, preencha os campos para prosseguir com a compra!");
-    }
-})
