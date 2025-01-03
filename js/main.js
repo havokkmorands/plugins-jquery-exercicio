7bdfeb17faa35deb657ecf36842bc9870b366789
Michael <miguelmotaitz@gmail.com>
2025-01-03 09:51:40 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,42 +1,60 @@
-$('#telefone').mascara('(00) 00000-0000', {
+$(document).ready(function() {
+    $('#carousel-imagens').slick({
+        autoplay: true, 
+    });
+
+
+$('#telefone').mask('(00) 00000-0000', {
     placeholder: '(DDD) 12345-6789'
 })
+});
 
-$('#cpf').mascara('000.000.000-00', {
+
+$('#cpf').mask('000.000.000-00', {
     placeholder: '123.456.789-00'
 })
+});
 
-$('#cep').mascara('00000-000', {
+
+$('#cep').mask('00000-000', {
     placeholder: '012345-678'
 })
+});
 
 $('form').validate({
     rules: {
         nome: {
-            requerido: true
+            
+            required: true
         },
         email: {
-            requerido: true,
+            
+            required: true,
             email: true
         },
         telefone: {
-            requerido: true
+            
+            required: true
         },
         endereco: {
-            requerido: true
+           
+            required: true
         },
         cep: {
-            requerido: true
+            
+            required: true
         },
         cpf: {
-            requerido: true
+            
+            required: true
         },
     },
     submitHandler: function (form) {
         alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
         form.reset();
     },
-    invalidHandler: function (form, validator) {
+    i
+    invalidHandler: function (evento, validador) {
         alert("Por favor, preencha os campos para prosseguir com a compra!");
     }
 })
