8e0948e12d0fa547804b1826ff440df070eedf8c
LDRMRQS <romulomarquesx@gmail.com>
2025-02-12 15:20:10 -0300

    mudando os nomes dos elementos e funções para que funcionem

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
 $(document).ready(function () {
 
-    $('#carousel').slick({
+    $('#carousel-imagens').slick({
         autoplay: true,
         arrows: false
     })
 
-    $('#telefone').mascara('(00) 00000-0000', {
+    $('#telefone').mask('(00) 00000-0000', {
         placeholder: '(DDD) 12345-6789'
     })
     
-    $('#cpf').mascara('000.000.000-00', {
+    $('#cpf').mask('000.000.000-00', {
         placeholder: '123.456.789-00'
     })
     
-    $('#cep').mascara('00000-000', {
+    $('#cep').mask('00000-000', {
         placeholder: '012345-678'
     })
     
     $('form').validate({
         rules: {
             nome: {
-                requerido: true
+                required: true
             },
             email: {
-                requerido: true,
+                required: true,
                 email: true
             },
             telefone: {
-                requerido: true
+                required: true
             },
             endereco: {
-                requerido: true
+                required: true
             },
             cep: {
-                requerido: true
+                required: true
             },
             cpf: {
-                requerido: true
+                required: true
             },
         },
         submitHandler: function (form) {
